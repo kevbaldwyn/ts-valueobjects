edd5717c8981d796f00d49da3f972c349c02d738
Kev Baldwyn <kev@kevbaldwyn.co.uk>
2020-12-06 22:48:59 +0000

    initial travis setup

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node"
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
 };
