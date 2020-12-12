a668721f43bb7229597447f67be00af29c8f6b73
Kev Baldwyn <kev@kevbaldwyn.co.uk>
2020-12-12 13:58:44 +0000

    feat: add BooleanScalar value object

---
+++ src/Scalars/BooleanScalar.ts
@@ -0,0 +1,36 @@
+import { ValueObject, getType } from "../ValueObject";
+
+export class BooleanScalar implements ValueObject<boolean> {
+  type: string;
+
+  value: boolean;
+
+  constructor(value: boolean) {
+    this.value = value;
+    this.type = getType(this, BooleanScalar, true);
+  }
+
+  public static fromNative(value: boolean): BooleanScalar {
+    return new this(value);
+  }
+
+  public static boo(): boolean {
+    return true;
+  }
+
+  public isSame = (object: ValueObject<boolean>): boolean => {
+    return this.type === object.type && this.value === object.value;
+  };
+
+  public toNative = (): boolean => {
+    return this.value;
+  };
+
+  public isTrue = (): boolean => {
+    return this.toNative();
+  };
+
+  public isFalse = (): boolean => {
+    return !this.toNative();
+  };
+}
